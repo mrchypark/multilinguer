a74f9972586607326939c397d69021f0521b4d2c
Chanyub Park <mrchypark@gmail.com>
2019-03-02 02:23:59 +0900

    linux untar

--- R/unfile.R
+++ R/unfile.R
@@ -4,6 +4,6 @@ uncorretto <- function(os) {
     Windowsx86 = utils::unzip,
     Windowsx64 = utils::unzip,
     Darwinx64 = function(tarfile, exdir, ...){utils::untar(tarfile, exdir, compressed = "gzip")},
-    Linuxx64 = "https://d2znqt9b1bc64u.cloudfront.net/amazon-corretto-8.202.08.2-linux-x64.tar.gz"
+    Linuxx64 = function(tarfile, exdir, ...){utils::untar(tarfile, exdir, compressed = "gzip")}
   )
 }
