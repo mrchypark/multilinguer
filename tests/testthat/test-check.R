735b66df5a9a050ac1c79f796b174f7eb9fe7e3c
Chanyub Park <mrchypark@gmail.com>
2019-05-10 22:28:15 +0900

    add test

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,10 +1,10 @@
 test_that("internal rJava check function", {
   print(multilinguer:::check_rJava())
-  testthat::expect_success(multilinguer:::check_rJava())
+  testthat::expect_visible(multilinguer:::check_rJava())
 })
 
 
 test_that("internal conda check function", {
   print(multilinguer:::check_conda())
-  testthat::expect_success(multilinguer:::check_conda())
+  testthat::expect_visible(multilinguer:::check_conda())
 })
