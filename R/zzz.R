2441e1d4435f34342ec5fcc3f805edf5118871df
Chanyub Park <mrchypark@gmail.com>
2019-05-11 22:41:30 +0900

    fix try osx

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,3 @@
-.mlgrEnv <- new.env()
-
-.onLoad <- function(libname, pkgname) {
-  os <- "os_class"
-  bits <- Sys.info()["machine"]
-  if (grepl("x86", bits) & grepl("64", bits)) {
-    bits <- "x64"
-  }
-  class(os) <- paste0(Sys.info()["sysname"], as.character(bits))
-  assign("os", os, envir = .mlgrEnv)
-}
-
 .onAttach <- function(libname, pkgname){
   packageStartupMessage("This package only support mac, win32, win64 for now.")
   packageStartupMessage("Please issue here https://github.com/mrchypark/multilinguer/issues.")
