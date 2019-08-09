32633382a8cd08b7099c3abb3210e3969675318b
Chanyub.park <mrchypark@gmail.com>
2019-08-09 11:50:36 +0900

    add force

--- R/has.R
+++ R/has.R
@@ -2,13 +2,17 @@
 #'
 #' Check if there is a conda, and install if it does not exist.
 #'
+#' @param force force installation. Defualt is FALSE
 #' @param update defualt is T with conda update.
 #' @param silent defualt is T.
 #' @param quiet defualt is F.
 #' @import conda4r
 #' @export
-has_conda <- function(update = TRUE, silent = T, quiet = F){
-  if (!conda4r::conda_available()) {
+has_conda <- function(force = FALSE,
+                      update = TRUE,
+                      silent = TRUE,
+                      quiet = FALSE){
+  if (!conda4r::conda_available() | force) {
     conda4r::install_conda(silent = silent, quiet = quiet)
   } else {
     return(TRUE)
@@ -17,7 +21,7 @@ has_conda <- function(update = TRUE, silent = T, quiet = F){
     if (!quiet) cat("Fix windows conda ssl issue\n")
     conda4r::fix_ssl_error()
   }
-  if (update){
+  if (update) {
     if (!quiet) cat("Update conda\n")
     conda4r::update_conda()
   }
@@ -28,11 +32,12 @@ has_conda <- function(update = TRUE, silent = T, quiet = F){
 #'
 #' Check if there is a java, and install if it does not exist.
 #'
+#' @param force force installation. Defualt is FALSE
 #' @import correttor
 #' @export
-has_java <- function(){
-  if (!correttor::java_available()) {
-    correttor::install_correttor()
+has_java <- function(force = FALSE){
+  if (!correttor::java_available() | force) {
+    correttor::install_corretto()
   } else {
     return(TRUE)
   }
