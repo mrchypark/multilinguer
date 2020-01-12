598de9b34ec26e73ca9a5888936e0915fd320546
Chanyub.Park <mrchypark@gmail.com>
2020-01-12 23:24:22 +0900

    [WIP] done download rust init

--- R/download_rust.R
+++ R/download_rust.R
@@ -1,3 +1,56 @@
+#' rust installer download
+#'
+#' @param os Type of OS
+#' @param dest where to download
+#' @importFrom utils download.file
+#' @export
+rs_download <- function(os, dest){
+  UseMethod("rs_download")
+}
+
+rs_download.default <- function(os){
+  stop("no method for ", class(os)[1L])
+}
+
+rs_download.Windowsx86 <- function(os, dest = rs_dest_loc()){
+  tar <- "https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe"
+  download.file(tar, destfile = dest, mode = "wb")
+}
+
+rs_download.Windowsx64 <- function(os, dest = rs_dest_loc()){
+  tar <- "https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-gnu/rustup-init.exe"
+  download.file(tar, destfile = dest, mode = "wb")
+}
+
+rs_download.Darwinx64 <- function(os, dest = rs_dest_loc()){
+  tar <- "https://static.rust-lang.org/rustup/dist/x86_64-apple-darwin/rustup-init"
+  download.file(tar, destfile = dest)
+}
+
+rs_download.Linuxx64 <- function(os, dest = rs_dest_loc()){
+  tar <- "https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"
+  download.file(tar, destfile = dest)
+}
+
+rs_download.Linuxx86<- function(os, dest = rs_dest_loc()){
+  tar <- "https://static.rust-lang.org/rustup/dist/i686-unknown-linux-gnu/rustup-init"
+  download.file(tar, destfile = dest)
+}
+
+#' @importFrom fs path path_temp path_home
+rs_dest_loc <- function() {
+  os <- get_os()
+  return(switch(
+    class(os),
+    Windowsx86 = fs::path(fs::path_temp(), "rustup-init.exe"),
+    Windowsx64 = fs::path(fs::path_temp(), "rustup-init.exe"),
+    Darwinx64 = fs::path(fs::path_temp(), "rustup-init.sh"),
+    Linuxx86 = fs::path(fs::path_temp(), "rustup-init.sh"),
+    Linuxx64 = fs::path(fs::path_temp(), "rustup-init.sh"),
+    message("not support os yet")
+  ))
+}
+
 #' Install Rust
 #'
 #' @export
