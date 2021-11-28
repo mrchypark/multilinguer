47d58d321a6a1e0a88b53e510bcc15811d340bee
chanyub Park <mrchypark@gmail.com>
2021-11-28 16:54:56 +0900

    java check

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -1,5 +1,4 @@
 test_that("install java and check", {
   install_jdk()
-  install.packages("rJava", repos = "https://cloud.r-project.org")
-  expect_silent(rJava::.jinit())
+  testthat::expect_true(java_available())
 })
