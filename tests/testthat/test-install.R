5e536ba7795b004167508fbf8ff84497c0f4a9cf
chanyub Park <mrchypark@gmail.com>
2021-12-09 01:28:27 +0900

    rm test for print os info

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -2,7 +2,3 @@ test_that("install java and check", {
   install_jdk()
   testthat::expect_true(java_available())
 })
-
-test_that("check os print", {
-  testthat::expect_equal(multilinguer:::get_os(), "")
-})
