097b76200c4dd801a3124adb1e12e26c7c76c769
chanyub Park <mrchypark@gmail.com>
2022-01-19 22:55:46 +0900

    rm debug print

--- R/jdk_install.R
+++ R/jdk_install.R
@@ -207,7 +207,6 @@ jdk_installer_unc <- function(installer, path) {
   }
 
   status <- unc(installer,  exdir = path)
-  print(status)
   if (status != 0) {
     unlink(path)
     usethis::ui_stop("Uncompress jdk failed. status: {usethis::ui_code(status)}")
